14dbd4f2b63b88d47fd1ecb740da4490ef25c6e8
Everglow <948719668@qq.com>
2020-09-24 20:00:36 +0800

    Initial commit

---
+++ src/test/java/com/everglow/accounting/AccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everglow.accounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
